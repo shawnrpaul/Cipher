05d92b5cc4ebda6ffeb8865c569f4921ecf2d755
Srpboyz <srpboyz69@gmail.com>
2023-06-01 00:12:22 -0700

    Removed Examples Extensions from LocalAppData

--- LocalAppData/Cipher/include/extension/Jedi/run.py
+++
@@ -1,5 +0,0 @@
-from .jedi import Jedi
-
-
-def run(*args, **kwargs) -> Jedi:
-    return Jedi(*args, **kwargs)
--- LocalAppData/Cipher/include/extension/RPC/run.py
+++
@@ -1,5 +0,0 @@
-from .rpc import RPC
-
-
-def run(*args, **kwargs) -> RPC:
-    return RPC(*args, **kwargs)
